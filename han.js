75787df8d4de6b7ad79ed3e993936cbc370dc960
Tan I-kun <chief@ethantw.net>
2011-05-10 13:44:36 -0700

    version 1.1

--- han.js
+++ han.js
@@ -1,149 +1,191 @@
 /* 漢字標準格式
  * Han, the CSS Reset
  *
- * Version: 1.0.0
+ * Version: 1.1
  * Lisence: MIT Lisence
- * Last-Modified: 2011/03/08
+ * Last Modified: 2011/05/10
  */
+
 
 (function ($) {
 
-    // MSIE HTML5 elements fixer
-    msie_html5();
+var han = {
+    js: function() {
+        this.msie_html5();
+        $(document).ready(function() {  han.init();  });
+    },
 
 
-    $(document).ready(function () {
-        var cond = condtions() + " ",
-            // conditions: OS and browser
+    init: function( range, dectect ) {
+        if ( !range && $("html").hasClass("no-han_init") )
+            return;
 
-            smoothyType = type_smoothing();
-            // type_smoothing
+        var hasFontSmoothing = han.support.font_smoothy,
+            hasTextEm = han.support.text_em,
+            hasQuotes = han.support.quotes,
+            dectector;
 
-        $("html")
-        .addClass("han-js " + cond + "hasFontSmoothing-" + (( smoothyType ) ? "true" : 
-            ( smoothyType == false ) ? "false" : "unknown")
-        );
+        if ( !range && dectect != false || range && dectect ) {
+            dectector = "han-js " + han.conditions();
+            dectector += " hasFontSmoothing-" + (( hasFontSmoothing ) ? "true" : ( hasFontSmoothing == false ) ? "false" : "unknown");
+            dectector += " " + (( !hasTextEm ) ? "no-" : "") + "textemphasis";
+            dectector += " " + (( !hasQuotes ) ? "no-" : "") + "quotes";
 
-		
-	// punctuation: CJK emphasis dots
-        $("em").each(function () {
-            $(this).html( cjk_spanify($(this).html() + "<!---->") );
+            $(( !range ) ? "html" : range).addClass(dectector);
+        }
+
+
+        var range = range || document;
+
+        // punctuation: CJK emphasis dots for <em>
+        $(range).find("em").each(function() {
+            $(this).html( han.characterize($(this).html(), {
+                cjk: ( hasTextEm ) ? "punctuation" : null,
+                latin: ( hasTextEm ) ? "none" : "individual" })
+            );
         });
 
 
-        // <q>
-        if ( $.browser.webkit )
-            $("q").each(function () {
-                if ( $(this).parents("q").length%2 != 0 )
-                    $(this).addClass("double");
+        // punctuation: CJK quotes for <q>
+        if ( !hasQuotes )
+            $(range).find("q q").each(function() {
+                if ( $(this).parents("q").length%2 != 0 )  $(this).addClass("double");
+            });
+
+
+        // <i>
+        if ( $.browser.msie )
+            $(range).find("i").each(function() {
+                $(this).html( han.characterize($(this).html(), { cjk: "none", latin: "group" }) );
             });
 
 
         // <ruby>
-        $("ruby").each(function () {
+        $(range).find("ruby.pinyin").addClass("romanization");
+        $(range).find("ruby.zhuyin").addClass("mps");
+
+        $(range).find("ruby").each(function() {
             if ( $(this).hasClass("mps") || !$.browser.webkit ) {
                   var html = $(this).html(),
-                      result = ( !$.browser.msie ) ?
-                      html.replace(/<\/rt>/ig, '</rt></span><span class="rb">') :
-                      html.replace(/<rt>/ig, '<span class="rt">').replace(/<\/rt>/ig, '</span></span><span class="rb">');
+                      result = html.replace(/<rt>/ig, '<span class="rt">').replace(/<\/rt>/ig, '</span></span><span class="rb">');
 
                 $(this).html('<span class="rb">' + result + '</span>');
             }
         });
 
         if ( !$.browser.webkit )
-            $("ruby.romanization rt, ruby.mps-ltr rt, ruby.romanization .rt, ruby.mps-ltr .rt").each(function () {
+            $("ruby.romanization .rt, ruby.mps-ltr .rt").each(function () {
                 var width = [];
-                width['rt'] = $(this).width();
-                width['rb'] = $(this).parent("span.rb").width();
+                width['rt'] = $(this).outerWidth();
+                width['rb'] = $(this).parent("span.rb").outerWidth();
     
                 $(this).css("min-width", width['rb']);
                 $(this).parent("span.rb").css("min-width", width['rt']);
             });
 
-        $("ruby.mps rt, ruby.mps .rt").each(function () {
-            var height = [];
+
+        $("ruby.mps .rt").each(function () {
+            /*var height = [];
             height['rt'] = $(this).height();
-            //height['rb'] = $(this).parent("span.rb").height();
+            height['rb'] = $(this).parent("span.rb").height();
 
-            $(this).parent("span.rb").css("min-height", height['rt']);
-
+            $(this).parent("span.rb").css("min-height", height['rt']);*/
             $(this).html(
                 $(this).html()
                     .replace(/(˙)/ig, '<span class="tone tone-0">$1</span>')
                     .replace(/([ˊˇˋ])/ig, '<span class="tone">$1</span>')
             );
         });
-
-    });
-
-
-/* Functions */
-function condtions ( q ) {
-    var os = (navigator.appVersion.indexOf("Mac") != -1) ? "mac" :
-        (navigator.appVersion.indexOf("X11") != -1) ? "unix" :
-        (navigator.appVersion.indexOf("Linux") != -1) ? "linux" :
-        (navigator.appVersion.indexOf("Win") != -1) ? "win" : "os-unknown",
-
-        browser = ($.browser.mozilla) ? "mozilla" :
-        ($.browser.opera) ? "opera" :
-        ($.browser.webkit) ? "webkit" :
-        ($.browser.msie && $.browser.version <= 7) ? "msie msie-old" :
-        ($.browser.msie && $.browser.version >= 8) ? "msie" : "browser-unknown";
-
-    return ( q === "os" ) ? os : ( q === "browser" ) ? browser : os + " " + browser;
-};
+    },
 
 
-// MSIE HTML5 elements fix
-// Copyright (c) 2010 Johan de Jong <http://johan.notitia.nl>
-// Creative Commons Attribution-Share Alike 3.0
-// http://creativecommons.org/licenses/by-sa/3.0/
-function msie_html5 ( tags ) {
-    // only execute when used in IE
-    if ( $.browser.msie && $.browser.version <= 8 ) {
-        // list of (known) HTML5 elements
-        e = "abbr,article,aside,audio,canvas,datalist,details,eventsource,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,rp,rt,ruby,section,summary,time,video";
+    characterize: function( content, glyph ) {
+        var content = content + "<!---->",
+            glyph = glyph || {};
+            glyph = {
+                cjk: glyph.cjk || "individual",
+                latin: glyph.latin || "group",
+                space: glyph.space || "individual"
+            };
 
-        // if tags is set, add them to the elements list
-        if ( tags )  e += ',' + tags;
+        // CJK
+        if ( glyph.cjk === "individual" )
+            content = content
+            .replace(/([\u1100-\u11FF\u2030-\u217F\u2600-\u261B\u2620-\u277F\u2E80-\u2FDF\u2FF0-\u4DBF\u4E00-\u9FFF\uA960-\uA97F\uAC00-\uD7AF\uD7B0-\uD7FF\uF900-\uFAFF\uFE30-\uFE4F\uFF00\uFF01\uFF03-\uFF06\uFF08-\uFF0C\uFF0E-\uFF19\uFF1F-\uFF3D\uFF40-\uFF5B\uFF5D-\uFFEF])(?=[^>]*<)/ig, '<span class="cjk">$1</span>')
+            .replace(/<span class="cjk">([·・︰、，。：；？！—⋯．·])/ig, '<span class="cjk punc">$1')
+            .replace(/<span class="cjk">([「『（〔【《〈“‘])/ig, '<span class="cjk punc open">$1')
+            .replace(/<span class="cjk">([」』）〕】》〉’”])/ig, '<span class="cjk punc close">$1');
 
-        e = e.split(',');
-        i = e.length;
-        // loop through the elements and add them to the DOM
-        while (i--) {
-            document.createElement(e[i]);
+        else if ( glyph.cjk === "group" )
+            content = content
+            .replace(/([\u1100-\u11FF\u2030-\u217F\u2600-\u261B\u2620-\u277F\u2E80-\u2FDF\u2FF0-\u4DBF\u4E00-\u9FFF\uA960-\uA97F\uAC00-\uD7AF\uD7B0-\uD7FF\uF900-\uFAFF\uFE30-\uFE4F\uFF00\uFF01\uFF03-\uFF06\uFF08-\uFF0C\uFF0E-\uFF19\uFF1F-\uFF3D\uFF40-\uFF5B\uFF5D-\uFFEF]+)(?=[^>]*<)/ig, '<span class="cjk">$1</span>');
+
+        else if ( glyph.cjk === "punctuation" )
+            content = content
+            .replace(/([·・︰、，。：；？！—⋯．·])(?=[^>]*<)/ig, '<span class="cjk punc">$1</span>')
+            .replace(/([「『（〔【《〈“‘])(?=[^>]*<)/ig, '<span class="cjk punc open">$1</span>')
+            .replace(/([」』）〕】》〉’”])(?=[^>]*<)/ig, '<span class="cjk punc close">$1</span>');
+
+
+        // Latin
+        if ( glyph.latin === "individual" )
+            content = content
+            .replace(/([\u0020-\u003B\u003D\u003F-\u007F\u0100-\u017F\u0180-\u024F\u0300-\u036F\u1E00-\u1EFF\u2000-\u206F\u2070-\u209F\u20A0-\u20CF\u2100-\u214F\u2150-\u218F])(?=[^>]*<)/ig, '<span class="latin">$1</span>');
+
+        else if ( glyph.latin === "group" )
+            content = content
+            .replace(/([\u0020-\u003B\u003D\u003F-\u007F\u0100-\u017F\u0180-\u024F\u0300-\u036F\u1E00-\u1EFF\u2000-\u206F\u2070-\u209F\u20A0-\u20CF\u2100-\u214F\u2150-\u218F]+)(?=[^>]*<)/ig, '<span class="latin">$1</span>');
+
+
+        // spaces
+        if ( glyph.space === "individual" )
+            content = content
+            .replace(/<span class="(\w+)">([\s])/ig, '<span class="$1 space">$2');
+
+        return content;
+    },
+
+
+    msie_html5: function(tags) {
+        // only execute when used in IE (<= 8)
+        if ( $.browser.msie && $.browser.version <= 8 ) {
+            // list of (known) HTML5 elements
+            e = "abbr,article,aside,audio,canvas,datalist,details,dfn,eventsource,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,rp,rt,ruby,section,summary,time,video";
+    
+            // if tags is set, add them to the elements list
+            if ( tags )  e += ',' + tags;
+    
+            e = e.split(',');
+            i = e.length;
+            // loop through the elements and add them to the DOM
+            while (i--) {
+                document.createElement(e[i]);
+            }
         }
+    },
+
+
+    conditions: function( q ) {
+        var appVersion = function( os ) {
+                return (navigator.appVersion.indexOf(os) != -1) ? true : false;
+            },
+
+            os = (appVersion("Mac")) ? "mac" : (appVersion("X11")) ? "unix" : (appVersion("Linux")) ? "linux" : (appVersion("Win")) ? "win"  : "os-unknown";
+
+            browser = ($.browser.mozilla) ? "mozilla" : ($.browser.opera) ? "opera" : ($.browser.webkit) ? "webkit" :
+                ($.browser.msie && $.browser.version <= 7) ? "msie msie-old ie-" + $.browser.version : ($.browser.msie && $.browser.version >= 8) ? "msie ie-"  + $.browser.version : "browser-unknown";
+
+        return ( q === "os" ) ? os : ( q === "browser" ) ? browser : os + " " + browser;
     }
 };
 
 
-function cjk_spanify ( html, glyph ) {
-    if ( glyph === "cjk" || !glyph )
-        html = html
-            .replace(/([\u1100-\u11FF\u2030-\u217F\u2600-\u261B\u2620-\u277F\u2E80-\u2FDF\u2FF0-\u4DBF\u4E00-\u9FFF\uA960-\uA97F\uAC00-\uD7AF\uD7B0-\uD7FF\uF900-\uFAFF\uFE30-\uFE4F\uFF00\uFF01\uFF03-\uFF06\uFF08-\uFF0C\uFF0E-\uFF19\uFF1F-\uFF3D\uFF40-\uFF5B\uFF5D-\uFFEF])(?=[^>]*<)/ig, '<span class="cjk">$1</span>')
-            // CJK
-            .replace(/<span class="cjk">([·・︰、，。：；？！—⋯．·])/ig, '<span class="cjk punc">$1')
-            // CJK punctuation
-            .replace(/<span class="cjk">([「『（〔【《〈“‘])/ig, '<span class="cjk punc open">$1')
-            // CJK punctuation (open bracket/quote)
-            .replace(/<span class="cjk">([」』）〕】》〉’”])/ig, '<span class="cjk punc close">$1');
-            // CJK punctuation (close bracket/quote)
 
-    if ( glyph === "latin" || !glyph )
-        html = html
-            .replace(/([\u0020-\u003B\u003D\u003F-\u007F\u0100-\u017F\u0180-\u024F\u0300-\u036F\u1E00-\u1EFF\u2000-\u206F\u2070-\u209F\u20A0-\u20CF\u2100-\u214F\u2150-\u218F]+)(?=[^>]*<)/ig, '<span class="latin">$1</span>');
-            // Latin
+var a = $('<a href="/" style="display: none; text-emphasis: dot; -moz-text-emphasis: dot; -o-text-emphasis: dot; -webkit-text-emphasis: dot;">tester</a>'),
+    q = $('<q style="display: none; quotes: \'“\' \'”\' \'‘\' \'’\'">tester</q>'),
+    fs = function() {
+    if ( han.conditions("os") === "mac" )  return true;
 
-    return html;
-};
-
-
-
-// TypeHelpers version 1.0
-// by Zoltan Hawryluk
-// https://gist.github.com/283689
-function type_smoothing () {
     // IE has screen.fontSmoothingEnabled - sweet!      
     if ( typeof (screen.fontSmoothingEnabled) !== "undefined" )
         return screen.fontSmoothingEnabled;  
@@ -176,8 +218,8 @@ function type_smoothing () {
                     var imageData = ctx.getImageData(i, j, 1, 1).data,
                         alpha = imageData[3];
           
-                    if (alpha != 255 && alpha != 0 && alpha > 180)
-                        return true; // font-smoothing must be on.
+                    if (alpha != 255 && alpha != 0 && alpha > 180)  return true;
+                    // font-smoothing must be on.
                 }
             }
 
@@ -193,5 +235,26 @@ function type_smoothing () {
     }
 };
 
-})(jQuery);
 
+han.support = {
+    font_smoothy: fs(),
+
+    text_em: ( /^dot$/.test( a.css("-webkit-text-emphasis-style") ) ||
+      /^dot$/.test( a.css("text-emphasis-style") ) ||
+      /^dot$/.test( a.css("-moz-text-emphasis-style") ) ||
+      /^dot$/.test( a.css("-o-text-emphasis-style") ) ) ? true : false,
+
+    quotes: /^"“" "”" "‘" "’"$/.test( q.css("quotes") )
+};
+
+
+han.js();
+
+window.han = han.external = {
+    applyTo: han.init,
+    characterize: han.characterize,
+    conditions: han.conditions,
+    support: han.support
+};
+
+})(jQuery);
