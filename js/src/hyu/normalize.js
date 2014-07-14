f49011638c0a8b4ea2d3665a62788abcb27f3a91
Chen Yijun <ethantw@me.com>
2014-07-14 12:41:18 +0800

    rename `LineDeco` to `DecoLine` for coherent grammar

--- js/src/hyu/normalize.js
+++ js/src/hyu/normalize.js
@@ -105,15 +105,15 @@ $.extend( Hyu, {
   //
   renderElem: function( context ) {
     this.renderRuby( context )
-    this.renderLineDeco( context )
-    this.renderLineDeco( context, 's, del' )
+    this.renderDecoLine( context )
+    this.renderDecoLine( context, 's, del' )
     this.renderEm( context )
   },
 
   // Traverse target elements (those with text-decoration
   // -line) to see if we should address spacing in
   // between for semantic presentation.
-  renderLineDeco: function( context, target ) {
+  renderDecoLine: function( context, target ) {
     var
       target = target || 'u, ins',
       $target = $.qsa( target, context ),
