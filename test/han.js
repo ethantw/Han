a61b5b1dea505b10164d144b44ebc604aec14bbd
Chen Yijun <ethantw@me.com>
2015-03-30 18:23:19 +0800

    Fix the issue that spaced pseudo-elem not collapsing properly on WebKit

--- test/han.js
+++ test/han.js
@@ -2127,6 +2127,18 @@ Han.renderJiya = function( context ) {
     ellinika:  'none',
     kirillica: 'none'
   })
+
+  // The reason we're doing this instead of using pseudo elements in CSS
+  // is because WebKit has problem rendering pseudo elements containing only 
+  // space.
+  $.qsa( 'char.biaodian.open, char.biaodian.end', context )
+  .forEach(function( elem ) {
+    var html = '<inner>' + elem.innerHTML + '</inner>'
+    var hcs = '<hcs> </hcs>'
+    var isOpen = elem.classList.contains( 'open' )
+    elem.innerHTML = isOpen ? hcs + html : html + hcs
+  })
+
   return finder
 }
 
